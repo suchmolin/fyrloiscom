b9a13be07ee3f3af309970f23d5edbb5d35f7a50
suchmolin <suchmolin11@gmail.com>
2025-01-07 11:49:32 -0400

    agregar gtm a una url simple

--- src/app/[locale]/contactUs/page.jsx
+++ src/app/[locale]/contactUs/page.jsx
@@ -1,3 +1,4 @@
+import { GoogleTagManager } from "@next/third-parties/google"
 import FAQSection from "@/components/FAQSection/page"
 import FormContactUs from "@/components/FormContactUs/page"
 import MasInfoContactUs from "@/components/MasInfoContactUs/page"
@@ -7,17 +8,20 @@ import { useTranslations } from "next-intl"
 export default function ContactUs() {
   const t = useTranslations("contactUs.MasInfoContactUs")
   return (
-    <div className="w-full flex flex-col items-center">
-      <FormContactUs />
-      <MasInfoContactUs
-        titulo={t("h2")}
-        subtitulo={t("p")}
-        data={data[0]}
-        mt="-mt-32"
-      />
-      <div className="w-full mt-10">
-        <FAQSection />
+    <>
+      <GoogleTagManager gtmId="AW-16807419951" />
+      <div className="w-full flex flex-col items-center">
+        <FormContactUs />
+        <MasInfoContactUs
+          titulo={t("h2")}
+          subtitulo={t("p")}
+          data={data[0]}
+          mt="-mt-32"
+        />
+        <div className="w-full mt-10">
+          <FAQSection />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
