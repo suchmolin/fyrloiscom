6ab0e00a78d2bcf335743bc5a640f1bc795e7c00
suchmolin <suchmolin11@gmail.com>
2025-01-07 10:45:12 -0400

    agregar etiqueta GTM

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { GoogleTagManager } from "@next/third-parties/google"
 import "./globals.css"
 export const metadata = {
   title: "Fyr Lois",
@@ -7,6 +8,7 @@ export const metadata = {
 export default async function RootLayout({ children, params: { locale } }) {
   return (
     <html lang={locale}>
+      <GoogleTagManager gtmId="GTM-T7XPF2MQ" />
       <body>{children}</body>
     </html>
   )
