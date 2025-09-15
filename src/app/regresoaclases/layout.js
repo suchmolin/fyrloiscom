3bd30fd8f65819e55092b69b5a231527c7f54017
suchmolin11 <suchmolin11@gmail.com>
2025-09-15 09:00:57 -0400

    redireccion de regreso a clases al 2025

--- src/app/regresoaclases/layout.js
+++ src/app/regresoaclases/layout.js
@@ -1,12 +1,10 @@
+import { redirect } from 'next/navigation'
+
 export const metadata = {
   title: "Fyr Lois: La Clave Para El Éxito de tu Hijo Es El Inglés",
   description: "Acompañamiento académico de primer nivel, adaptado a su etapa y competencias en inglés, durante todo el año escolar.",
 }
 
 export default function AnoEscolarLayout({ children }) {
-  return (
-    <html translate="no" lang="es"  suppressHydrationWarning>
-      <body>{children}</body>
-    </html>
-  )
+  redirect('https://regresoaclases.fyrlois.com')
 }
